873d66ee935e764ccd09a623f4fff430b4cfccc1
haris265 <harisahmedshaikh12@gmail.com>
2022-08-01 14:47:05 +0500

    done live changes

--- app/Http/Controllers/Api/Client/SearchController.php
+++ app/Http/Controllers/Api/Client/SearchController.php
@@ -22,8 +22,8 @@ class SearchController extends Controller
         if ($validator->fails()) {
             return response()->json(['status' => false, 'errors' => $validator->errors()]);
         }
-        $results = Person::where('person_A', 'like',"%{$request->$request->person_A}%")
-        ->where('person_B', 'like',"%{$request->$request->person_B}%")->get();
+        $results = Person::where('person_A', 'like',"{$request->person_A}%")
+        ->where('person_B', 'like',"{$request->person_B}%")->get();
         return response()->json(['status' => true, 'results' => $results]);
     }
 }
